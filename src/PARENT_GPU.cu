1039e86d61739a15f8efd4d70e5e36b8ecdf9d9f
Markus Fleck <fleck.markus@gmx.at>
2020-09-22 18:34:54 +0200

    Implemented GPU_RAM_Block class
    A class mapping a chunk of CPU RAM to a chunk of GPU RAM. If the
    corresponding degrees of freedom are needed for computation,
    they can be loaded to the GPU by calling the load_GPU() method.

--- src/PARENT_GPU.cu
+++ src/PARENT_GPU.cu
@@ -1,10 +1,63 @@
 #include <iostream>
 
+#define PRECISION double
+
+#define gpuErrchk(ans) { gpuAssert((ans), __FILE__, __LINE__); }
+inline void gpuAssert(cudaError_t return_code, const char *file, int line)
+{
+   if (return_code != cudaSuccess) 
+   {
+      fprintf(stderr,"GPUassert: %s %s %d\n", cudaGetErrorString(return_code), file, line);
+      exit(return_code);
+   }
+}
+
 using namespace std;
+
+class GPU_RAM_Block{
+	public:
+		unsigned char type;
+		unsigned int first_dof;
+		unsigned int last_dof;
+		unsigned int n_dofs;
+		PRECISION *ram_start;
+		PRECISION *ram_end; 
+		PRECISION *gpu_ram_start;
+	       	PRECISION *gpu_ram_end;
+		unsigned long long int n_bytes;
+
+	GPU_RAM_Block(unsigned char type, unsigned int first_dof, unsigned int n_dofs, 
+			PRECISION *ram_start, PRECISION *gpu_ram_start, unsigned long long int n_bytes){
+		
+		this->type = type;
+		this->first_dof = first_dof;
+		this->n_dofs = n_dofs;
+		this->last_dof = first_dof + n_dofs - 1;
+		this->ram_start = ram_start;
+		this->ram_end = ram_start + (n_bytes - 1) / sizeof(PRECISION);
+		this->gpu_ram_start = gpu_ram_start;
+		this->gpu_ram_end = gpu_ram_start + (n_bytes - 1) / sizeof(PRECISION);
+		this->n_bytes = n_bytes;
+
+	};
+
+
+	void load_GPU(){
+		gpuErrchk(cudaMemcpy(ram_start, gpu_ram_start, n_bytes, cudaMemcpyHostToDevice));
+	};	
+
+
+};
+
+
+
+
+
+
+
 
 int main(){
 
-cout<<"Backbone"<<endl;
 
 return 0;
 }
