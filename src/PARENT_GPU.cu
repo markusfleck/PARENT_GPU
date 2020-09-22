48df18dfcee4953bf86749d06f0384a16820ef12
Markus Fleck <fleck.markus@gmx.at>
2020-09-22 20:24:49 +0200

    Improved variable names in the GPU_RAM_Block class

--- src/PARENT_GPU.cu
+++ src/PARENT_GPU.cu
@@ -20,31 +20,32 @@ class GPU_RAM_Block{
 		unsigned int first_dof;
 		unsigned int last_dof;
 		unsigned int n_dofs;
-		PRECISION *ram_start;
-		PRECISION *ram_end; 
+		PRECISION *cpu_ram_start;
+		PRECISION *cpu_ram_end; 
 		PRECISION *gpu_ram_start;
 	       	PRECISION *gpu_ram_end;
 		unsigned long long int n_bytes;
 
 	GPU_RAM_Block(unsigned char type, unsigned int first_dof, unsigned int n_dofs, 
-			PRECISION *ram_start, PRECISION *gpu_ram_start, unsigned long long int n_bytes){
+			PRECISION *cpu_ram_start, PRECISION *gpu_ram_start, unsigned long long int n_bytes){
 		
 		this->type = type;
 		this->first_dof = first_dof;
 		this->n_dofs = n_dofs;
 		this->last_dof = first_dof + n_dofs - 1;
-		this->ram_start = ram_start;
-		this->ram_end = ram_start + (n_bytes - 1) / sizeof(PRECISION);
+		this->cpu_ram_start = cpu_ram_start;
+		this->cpu_ram_end = cpu_ram_start + (n_bytes - 1) / sizeof(PRECISION);
 		this->gpu_ram_start = gpu_ram_start;
 		this->gpu_ram_end = gpu_ram_start + (n_bytes - 1) / sizeof(PRECISION);
 		this->n_bytes = n_bytes;
 
-	};
+	}
 
 
 	void load_GPU(){
-		gpuErrchk(cudaMemcpy(ram_start, gpu_ram_start, n_bytes, cudaMemcpyHostToDevice));
-	};	
+		gpuErrchk(cudaMemcpy(cpu_ram_start, gpu_ram_start, n_bytes, cudaMemcpyHostToDevice));
+	}	
+
 
 
 };
