7f8a38483c4b36a3cccb325571031a91508d9527
Markus Fleck <fleck.markus@gmx.at>
2020-09-23 17:14:20 +0200

    Implemented CPU_RAM_Layout class
    Analogous to the GPU_RAM_Layout class, this class calculates the
    maximum number of degrees of freedom it can hold in RAM and subsequently
    sets up the partitioning of the allocated CPU RAM via pointers.

--- src/PARENT_GPU.cu
+++ src/PARENT_GPU.cu
@@ -83,20 +83,63 @@ class GPU_RAM_Layout
 
 		GPU_RAM_Layout(unsigned int n_frames, unsigned int n_bins, unsigned long long int gpu_n_bytes, char* gpu_ram_start)
 		{
-			//calculate the maximum number of dofs for one of the two dof_blocks we can use so that everything still fits into GPU RAM 
+			// calculate the maximum number of dofs (for one of the two dof_blocks) so that everything still fits into GPU RAM 
 			double a = (2 * n_frames - 1) * sizeof(PRECISION) - sizeof(unsigned int);
 			double b = (n_bins * n_bins + 2) * sizeof(unsigned int)	+ 2 * sizeof(PRECISION);
 			this->dofs_per_block = (unsigned int)( (-a/2 + sqrt(a * a / 4 + gpu_n_bytes * b) ) /b);		
 
-			//set the pointers for partitioning the GPU RAM according to the calculated dofs_per_block
+			// set the pointers for partitioning the GPU RAM according to the calculated dofs_per_block
 			dof_block_1 = (PRECISION*) gpu_ram_start;
 			dof_block_2 = dof_block_1 + dofs_per_block * n_frames;
 			result_fwd_1 = dof_block_2 + dofs_per_block * n_frames;
 			result_fwd_2 = result_fwd_1 + dofs_per_block * (dofs_per_block - 1);
 			result_all2all =  result_fwd_2 + dofs_per_block * (dofs_per_block - 1);
 			occupied_bins = (unsigned int*) (result_all2all + dofs_per_block * dofs_per_block);
-			histograms = occupied_bins + (2 * dofs_per_block - 1) * dofs_per_block * sizeof(unsigned int);
+			histograms = occupied_bins + (2 * dofs_per_block - 1) * dofs_per_block;
 		}
+};
+
+class CPU_RAM_Layout
+{
+	public:
+		unsigned int dofs_per_block;
+		PRECISION* dof_block_1;
+		PRECISION* dof_block_2;
+		PRECISION* result_entropy1D;
+		PRECISION* result_entropy2D;
+		PRECISION* extrema;
+		PRECISION* tmp_result_entropy;
+		unsigned int* tmp_result_occupied_bins;
+		double* tmp_read;
+
+		CPU_RAM_Layout(unsigned int n_frames, unsigned long long int cpu_n_bytes, char* cpu_ram_start, unsigned int gpu_dofs_per_block, unsigned int n_dofs_total)
+		{
+			// calculate the maximum number of dofs (for one of the two dof_blocks) so that everything still fits into CPU RAM
+			dofs_per_block = (unsigned int)( ( cpu_n_bytes - n_dofs_total * ( ( n_dofs_total + 3 ) * sizeof(PRECISION) + sizeof(double) ) 
+							+ ( 2 * gpu_dofs_per_block - 1 ) * gpu_dofs_per_block * ( sizeof(PRECISION) + sizeof(unsigned int) ) )
+							/ ( 2 * n_frames * sizeof(PRECISION) ) );
+			if(dofs_per_block < gpu_dofs_per_block)
+			{
+				cerr<<"WARNING: You probably have a GPU with a lot of RAM but your CPU RAM is rather small. ";
+				cerr<<"I recommend to get more CPU RAM, as this should significantly enhance performance."<<endl;	
+			}
+			// if all dofs fit into RAM, still set up two blocks to be consistent with the algorithm
+			if(2 * dofs_per_block >= n_dofs_total){
+				dofs_per_block = n_dofs_total / 2;
+				dofs_per_block += n_dofs_total % 2;
+			}
+
+			// set the pointers for partitioning the CPU RAM according to the calculated dofs_per_block
+			dof_block_1 = (PRECISION*) cpu_ram_start;
+			dof_block_2 = dof_block_1 + dofs_per_block * n_frames;
+			result_entropy1D = dof_block_2 + dofs_per_block * n_frames;
+			result_entropy2D = result_entropy1D + n_dofs_total;
+			extrema = result_entropy2D + n_dofs_total * (n_dofs_total - 1) / 2;
+			tmp_result_entropy = extrema + 2 * n_dofs_total;
+			tmp_result_occupied_bins = (unsigned int*)(tmp_result_entropy + (2 * gpu_dofs_per_block - 1) * gpu_dofs_per_block);
+			tmp_read = (double*)(tmp_result_occupied_bins + (2 * gpu_dofs_per_block - 1) * gpu_dofs_per_block);
+		}
+
 };
 
 
@@ -110,16 +153,20 @@ class RAM{
                 char *gpu_ram_end;
                 unsigned long long int gpu_n_bytes;
 		GPU_RAM_Layout* gpu_ram_layout;
+		CPU_RAM_Layout* cpu_ram_layout;
+		unsigned int n_dofs_total;
 
 		RAM(unsigned long long int cpu_n_bytes, unsigned long long int gpu_n_bytes, unsigned int n_dihedrals, unsigned int n_frames, unsigned int n_bins)
 		{
-			this->cpu_ram_start = new char [cpu_n_bytes];
-			this->cpu_ram_end = cpu_ram_start + cpu_n_bytes - 1;
+			cpu_ram_start = new char [cpu_n_bytes];
+			cpu_ram_end = cpu_ram_start + cpu_n_bytes - 1;
                         this->cpu_n_bytes = cpu_n_bytes; 
 			gpuErrchk( cudaMalloc((void**) &gpu_ram_start, gpu_n_bytes) );
-                        this->gpu_ram_end = gpu_ram_start + gpu_n_bytes - 1;
+                        gpu_ram_end = gpu_ram_start + gpu_n_bytes - 1;
                         this->gpu_n_bytes = gpu_n_bytes;
-			this->gpu_ram_layout = new GPU_RAM_Layout(n_frames, n_bins, gpu_n_bytes, gpu_ram_start);
+			gpu_ram_layout = new GPU_RAM_Layout(n_frames, n_bins, gpu_n_bytes, gpu_ram_start);
+			n_dofs_total = 3 * (n_dihedrals + 1);
+			cpu_ram_layout = new CPU_RAM_Layout(n_frames, cpu_n_bytes, cpu_ram_start, gpu_ram_layout->dofs_per_block, n_dofs_total);
 		}
 
 
@@ -134,10 +181,13 @@ unsigned long long int cpu_ram_available = static_cast<unsigned long long int>(1
 unsigned long long int gpu_ram_available = static_cast<unsigned long long int>(1024)*1024*1024*1;
 
 
-RAM ram(cpu_ram_available, gpu_ram_available, 10, 8.0e5, 50);
+RAM ram(cpu_ram_available, gpu_ram_available, 329, 8.0e5, 50);
 
 
-cout<<ram.gpu_ram_layout->dofs_per_block<<endl;
+cout<<ram.gpu_ram_layout->dofs_per_block<<" "<<ram.cpu_ram_layout->dofs_per_block<<endl;
+
+ram.cpu_ram_layout->tmp_result_occupied_bins[0] = 1;
+
 
 return 0;
 }
