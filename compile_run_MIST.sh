848cdda15e3b2cb7f9fc2b0fc5b7c14718c9ac41
Markus Fleck <fleck.markus@gmx.at>
2020-10-05 18:31:11 +0200

    Maximum information spanning tree approx. working
    src/MIST_GPU/MIST_GPU.cu now works. The code still needs
    cleanup as well as speed improvements.

--- compile_run_MIST.sh
+++ compile_run_MIST.sh
@@ -32,5 +32,6 @@ g++ --std=c++11 -O3 src/process_output/get_values_from_PAR.cpp obj/io.o obj/util
 
 
 nvcc --std=c++11 -O3 -Xptxas -O3 -gencode=arch=compute_61,code=\"sm_61,compute_61\" src/MIST_GPU/MIST_GPU.cu obj/io.o obj/util.o -o bin/MIST_GPU \
-&& bin/MIST_GPU -f output/UBM2_1.par -o output/UBM2_1_MIST.par
+&& bin/MIST_GPU -f output/UBM2_1.par -o output/UBM2_1_MIST.par \
+&& bin/get_values_from_PAR -p output/UBM2_1_MIST.par --short 
 
