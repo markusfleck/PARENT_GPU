846b99c9f7afcfc0036cbfae8f94dd5272b5270a
Markus Fleck <fleck.markus@gmx.at>
2020-10-14 10:08:11 +0200

    Removed unused kernels from PARENT_GPU_kernels.cu
    Kernels for 1D entropy calculations are not used,
    as their workload as negligible compared to the 2D
    entropy calculations. Therefore, those calculations
    are done on the cpu, at least for now. Furthermore,
    there were development kernels for calculating the
    p lnp sum. 98% of the GPU kernel time is spent on
    the histogram calculations. Therefore, optimizing
    the p lnp sum kernels has low priority for now.

--- src/PARENT_GPU/PARENT_GPU_kernels.cu
+++ src/PARENT_GPU/PARENT_GPU_kernels.cu
@@ -12,148 +12,6 @@
 #endif
 
 
-__global__ void cu_bondEnt(PRECISION *d_bondsEntropy1DMaster,
-                           PRECISION *d_minBonds, PRECISION *d_maxBonds,
-                           PRECISION *d_bondsChunk, const int nBonds,
-                           const int bDens1D, const int numFrames) {
-
-  int idx =
-      blockIdx.x * blockDim.x + threadIdx.x; // points to the number of the bond
-  if (idx > nBonds - 1)
-    return;
-  d_bondsEntropy1DMaster[idx] = 0;
-
-  PRECISION binsize, probDens, blen, plnpsum;
-  int occupbins;
-
-  extern __shared__ int histo[];
-  int offset_histo = threadIdx.x * bDens1D;
-
-  for (int k = 0; k < bDens1D; k++) {
-    histo[offset_histo + k] = 0; // initialize a histogram with zeros
-  }
-
-  binsize = (d_maxBonds[idx] - d_minBonds[idx]) /
-            bDens1D; // and calculate the size of the bins
-  for (int i = 0; i < numFrames;
-       i++) { // and fill the histogram using all frames of the trajectory
-    histo[offset_histo +
-          int((d_bondsChunk[idx * numFrames + i] - d_minBonds[idx]) /
-              binsize)] += 1;
-  }
-
-  occupbins = 0; // then use the histogram to calculate the (discretized)
-                 // entropy, taking care of the Jacobian
-  plnpsum = 0;
-  blen = d_minBonds[idx] + (binsize / 2.0);
-  for (int k = 0; k < bDens1D; k++) {
-    probDens = histo[offset_histo + k] / (numFrames * binsize * blen * blen);
-    if (probDens > 0) {
-      plnpsum = plnpsum + blen * blen * probDens * LOG(probDens);
-      occupbins = occupbins + 1;
-    }
-    blen += binsize;
-  }
-  plnpsum = -plnpsum * binsize;
-  d_bondsEntropy1DMaster[idx] =
-      plnpsum + (occupbins - 1.0) /
-                    (2.0 * numFrames); // and apply Herzel entropy unbiasing
-}
-
-__global__ void cu_angleEnt(PRECISION *d_anglesEntropy1DMaster,
-                            PRECISION *d_minAngles, PRECISION *d_maxAngles,
-                            PRECISION *d_anglesChunk, const int nAngles,
-                            const int aDens1D, const int numFrames) {
-
-  int idx = blockIdx.x * blockDim.x +
-            threadIdx.x; // points to the number of the angle
-  if (idx > nAngles - 1)
-    return;
-  d_anglesEntropy1DMaster[idx] = 0;
-
-  PRECISION binsize, probDens, theta, plnpsum;
-  int occupbins;
-
-  extern __shared__ int histo[];
-  int offset_histo = threadIdx.x * aDens1D;
-
-  for (int k = 0; k < aDens1D; k++) {
-    histo[offset_histo + k] = 0; // initialize a histogram with zeros
-  }
-
-  binsize = (d_maxAngles[idx] - d_minAngles[idx]) /
-            aDens1D; // and calculate the size of the bins
-  for (int i = 0; i < numFrames;
-       i++) { // and fill the histogram using all frames of the trajectory
-    histo[offset_histo +
-          int((d_anglesChunk[idx * numFrames + i] - d_minAngles[idx]) /
-              binsize)] += 1;
-  }
-
-  occupbins = 0; // then use the histogram to calculate the (discretized)
-                 // entropy, taking care of the Jacobian
-  plnpsum = 0;
-  theta = d_minAngles[idx] + (binsize / 2.0);
-  for (int k = 0; k < aDens1D; k++) {
-    probDens = histo[offset_histo + k] / (numFrames * binsize * SIN(theta));
-    if (probDens > 0) {
-      plnpsum = plnpsum + SIN(theta) * probDens * LOG(probDens);
-      occupbins = occupbins + 1;
-    }
-    theta += binsize;
-  }
-  plnpsum = -plnpsum * binsize;
-  d_anglesEntropy1DMaster[idx] =
-      plnpsum + (occupbins - 1.0) /
-                    (2.0 * numFrames); // and apply Herzel entropy unbiasing
-}
-
-__global__ void
-cu_dihedralEnt(PRECISION *d_dihedralsEntropy1DMaster, PRECISION *d_minDihedrals,
-               PRECISION *d_maxDihedrals, PRECISION *d_dihedralsChunk,
-               const int nDihedrals, const int dDens1D, const int numFrames) {
-
-  int idx = blockIdx.x * blockDim.x +
-            threadIdx.x; // points to the number of the dihedral
-  if (idx > nDihedrals - 1)
-    return;
-  d_dihedralsEntropy1DMaster[idx] = 0;
-
-  PRECISION binsize, probDens, plnpsum;
-  int occupbins;
-
-  extern __shared__ int histo[];
-  int offset_histo = threadIdx.x * dDens1D;
-
-  for (int k = 0; k < dDens1D; k++) {
-    histo[offset_histo + k] = 0; // initialize a histogram with zeros
-  }
-
-  binsize = (d_maxDihedrals[idx] - d_minDihedrals[idx]) /
-            dDens1D; // and calculate the size of the bins
-  for (int i = 0; i < numFrames;
-       i++) { // and fill the histogram using all frames of the trajectory
-    histo[offset_histo +
-          int((d_dihedralsChunk[idx * numFrames + i] - d_minDihedrals[idx]) /
-              binsize)] += 1;
-  }
-
-  occupbins = 0; // then use the histogram to calculate the (discretized)
-                 // entropy, taking care of the Jacobian
-  plnpsum = 0;
-  for (int k = 0; k < dDens1D; k++) {
-    probDens = histo[offset_histo + k] / (numFrames * binsize);
-    if (probDens > 0) {
-      plnpsum = plnpsum + probDens * LOG(probDens);
-      occupbins = occupbins + 1;
-    }
-  }
-  plnpsum = -plnpsum * binsize;
-  d_dihedralsEntropy1DMaster[idx] =
-      plnpsum + (occupbins - 1.0) /
-                    (2.0 * numFrames); // and apply Herzel entropy unbiasing
-}
-
 __global__ void histo2D(PRECISION* __restrict__ traj1, PRECISION* __restrict__ traj2, const int numFrames,
                         unsigned int* __restrict__ histo, const int n_bins,
                         PRECISION binSize1, PRECISION binSize2, PRECISION min1,
@@ -183,7 +41,6 @@ __global__ void histo2D_shared_block(PRECISION* __restrict__ traj1, PRECISION* _
     int n_bins_total = n_bins * n_bins;
                         
     while(tid < n_bins_total){
-        //~ printf("tid: %d n_bins_total: %d histo_block %d\n", tid, n_bins_total, histo_block[tid]);
         histo_block[tid] = 0;
         tid += blockDim.x;
     }
@@ -198,15 +55,8 @@ __global__ void histo2D_shared_block(PRECISION* __restrict__ traj1, PRECISION* _
   
   __syncthreads();
   
-  //~ unsigned int tmp = 0;
-  //~ for(unsigned int i = 0; i < n_bins_total; i++){
-    //~ tmp += histo_block[i];
-  //~ }
-  //~ printf("n_histo: %d\n", tmp);
-  
   tid = threadIdx.x;
   while(tid < n_bins_total){
-    //~ printf("tid: %d n_bins_total: %d histo_block %d\n", tid, n_bins_total, histo_block[tid]);
     atomicAdd(&(histo[tid]), histo_block[tid]);
     tid += blockDim.x;
   }
@@ -332,89 +182,4 @@ __global__ void cu_ddEnt(unsigned int *histo, const int numFrames,
   }
 }
 
-__global__ void cu_ddEnt1(unsigned int *histo, const int numFrames, const int bins, PRECISION binSize1, PRECISION binSize2, PRECISION *plnpsum, unsigned int *occupbins) {
-
-  int idx = threadIdx.x + blockIdx.x * blockDim.x;
-  int offset = blockDim.x * gridDim.x;
-  while (idx < bins * bins) {
-    PRECISION probDens = histo[idx] / (numFrames * binSize1 * binSize2);
-    if (probDens > 0) {
-      atomicAdd(plnpsum, probDens * LOG(probDens) * binSize1 * binSize2);
-      atomicAdd(occupbins, 1);
-    }
-    idx += offset;
-  }
-}
-
-__global__ void map_ddEnt(unsigned int *histo, const int n_frames, const int n_elements, PRECISION bin_size1, PRECISION bin_size2)  {
-    unsigned int idx_g = blockIdx.x*blockDim.x + threadIdx.x;
-    
-    PRECISION n_frames_inv; 
-    PRECISION bin_inv;
-    PRECISION tmp = histo[idx_g];
-    
-    if(idx_g < n_elements){
-        n_frames_inv = 1.0 / n_frames; //TODO: maybe hand over n_frames_inv as function argument 
-        bin_inv = 1.0 / (bin_size1 * bin_size2); // same here
-        tmp = histo[idx_g] * n_frames_inv;
-        histo[idx_g] = (tmp > 0) ? tmp * LOG( tmp * bin_inv) : 0;
-    }
-}
-
-
-__global__ void cu_ddEnt2(unsigned int *histo, const int n_frames, const int n_elements, PRECISION bin_size1, PRECISION bin_size2, PRECISION *plnpsum_out, unsigned int *occupbins_out) 
-{
-    extern __shared__ PRECISION shared_mem[];
-
-    unsigned int tid = threadIdx.x;
-    unsigned int n_threads_total = blockDim.x*gridDim.x;
-    unsigned int warp_mask = (0xffffffff);
-    unsigned int idx_g = blockIdx.x*blockDim.x + threadIdx.x;
-    
-    PRECISION n_frames_inv = 1.0 / n_frames; //TODO: maybe hand over n_frames_inv as function argument 
-    PRECISION bin_inv = 1.0 / (bin_size1 * bin_size2); // same here
-
-    unsigned int occupbins = 0;
-    PRECISION plnpsum = 0;
-    PRECISION tmp = histo[idx_g];
-    if(tmp > 0){
-        tmp *= n_frames_inv;
-        plnpsum = tmp * LOG( tmp * bin_inv);
-        occupbins++;
-    }
-    
-    idx_g += n_threads_total;
-    while(idx_g < n_elements){
-        tmp = histo[idx_g];
-        if(tmp > 0){
-            tmp *= n_frames_inv;
-            plnpsum += tmp * LOG( tmp * bin_inv );
-            occupbins++;
-        }
-        idx_g += n_threads_total;
-    }
-    
-    for (unsigned int delta = warpSize/2; delta > 0; delta /= 2)
-    {
-        plnpsum += __shfl_down_sync(warp_mask, plnpsum, delta);
-    }
-    
-    if((tid % warpSize) == 0)
-    {
-        shared_mem[tid/warpSize] = plnpsum;
-    }
-    __syncthreads();
-    
-    unsigned int n_to_process = (blockDim.x/warpSize) > 0 ? (blockDim.x/warpSize) : 1;
-    if (tid < n_to_process){
-        plnpsum = shared_mem[tid];
-        for (unsigned int delta = warpSize/2; delta > 0; delta /= 2){
-            plnpsum += __shfl_down_sync(warp_mask, plnpsum, delta);
-        }
-    }
-    if (tid == 0){
-        plnpsum_out[blockIdx.x] = plnpsum;
-        occupbins_out[blockIdx.x] = occupbins;
-    }
-}
 
