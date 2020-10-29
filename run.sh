38f12bc58b1b48d886f22646a849446736e55786
Markus Fleck <fleck.markus@gmx.at>
2020-10-29 03:22:28 +0100

    Added publication ready run.sh
    A fully commented and publication ready run.sh is provided.
    It does the compilation by using the Makefile mechanism.
    The script uses the protein in test_system, just like "make checks"
    does (therefore, using run.sh without according modifications, the
    protein is calculated twice). compile_run.sh was removed from the repo.

---
+++ run.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+CPU_RAM="8.0" # The CPU RAM you would like to provide in GiB. Be somewhat conservative here. E. g., if your machine has 32 GB RAM, set 25  
+GPU_RAM="2.0" # The GPU RAM you would like to provide in GiB. Be a litte conservative here. E. g., if your GPU has 6 GB RAM, set 5.25
+CUDA_ARCH=61 # The CUDA capability of your GPU, normally the higher the better. In doubt, leave it as it is: 61 is the minimum required
+export CUDA_VISIBLE_DEVICES=0 # If you have more than one GPUs, you can choose which one to use here
+export OMP_NUM_THREADS=4 # Set the number of threads of your CPU.
+
+IN_NAME="test_system/UBQ_UBM2" # The name and location of your trajectory (.xtc) and topology (.top) files
+OUT_NAME="output/UBQ_UBM" # The name and location of the output
+
+# make clean # remember to recompile if you change machines (e. g. when using cloud computing services)
+make CUDA_ARCH=${CUDA_ARCH}
+make checks; echo -e "\n\n\n" # run checks to make sure your system produces correct results 
+
+# rm -r output # remember that files will be overwritten without a warning
+mkdir output
+
+bin/BAT_builder -t ${IN_NAME}.top -x ${IN_NAME}.xtc -o ${OUT_NAME}.bat -bb "CA C N H1 O1" # convert the trajectory from GROMACS .xtc to PARENT/PARENT_GPU .bat format. Remember that .bat files are generally large, so make sure you provide enough harddisk space  
+bin/convert_BAT_to_GBAT -f ${OUT_NAME}.bat -o ${OUT_NAME}.gbat --ram $CPU_RAM # optionally, convert .bat to .gbat, tremendously enhancing harddisk reading times. Useful for large molecules/trajectories. 
+
+
+./bin/PARENT_GPU -f ${OUT_NAME}.gbat -o ${OUT_NAME}.par -b 50 --cpu_ram $CPU_RAM --gpu_ram $GPU_RAM && bin/get_values_from_PAR -p ${OUT_NAME}.par --short # run the MIE calculation, the heart of PARENT_GPU 
+echo -e "\n\n\n"
+
+bin/MIST_GPU -f ${OUT_NAME}.par -o ${OUT_NAME}_MIST_GPU.par && bin/get_values_from_PAR -p ${OUT_NAME}_MIST_GPU.par --short # calculate the MIST approximation (in principle optional, but numerically mandatory)
+# echo -e "\n\n\n"; bin/MIST_openMP -f ${OUT_NAME}.par -o ${OUT_NAME}_MIST_openMP.par && bin/get_values_from_PAR -p ${OUT_NAME}_MIST_openMP.par --short # no need to run this line unless for some exotic reason you don't want to use your GPU to calculate the MIST approximation as done just above
+
+
